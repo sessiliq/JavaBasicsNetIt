fafc7bbd8c862ec40b6df612a2bf55588ad7550c
penowa <penowa98@gmail.com>
2023-11-11 19:29:28 +0200

    new exercise from softuni - Count the words in a given string

---
+++ src/main/java/Homeworks/Homework0611ThirdTask.java
@@ -0,0 +1,22 @@
+package Homeworks;
+
+import java.util.Scanner;
+
+/*
+Count the words in a given string
+ */
+public class Homework0611ThirdTask {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please, enter sentence");
+        String sentence = input.nextLine();
+
+        int countWords = sentence.split("\\s+").length;
+
+        System.out.printf("In this sentence are %d word.\n", countWords);
+
+        //ако между думата и точката има спейс, брои точката за отделна дума.е
+    }
+}
