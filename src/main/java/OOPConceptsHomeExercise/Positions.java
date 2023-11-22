96d4f510011543b5b0f89eab773fc21823b9026d
penowa <penowa98@gmail.com>
2023-11-22 17:49:57 +0200

    getter and setter exercise

---
+++ src/main/java/OOPConceptsHomeExercise/Positions.java
@@ -0,0 +1,51 @@
+package OOPConceptsHomeExercise;
+
+import java.util.Scanner;
+
+public class Positions {
+    //private variables for the class
+    private String name;
+    private String position;
+    private int age;
+
+    //constructor
+    Positions(String name, String position, int age){
+        this.name = name;
+        this.position = position;
+        this.age = age;
+    }
+    //methods for setting values of the private variables
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setPosition(String newPosition){
+        this.position = newPosition;
+    }
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+    //methods for getting private variables values
+    public String getName(){
+        return name;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public int getAge(){
+        return age;
+    }
+    //method for accepting data from the console
+    public void acceptDataFromConsole(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please, enter name:");
+        setName(input.nextLine());
+        System.out.println("Please, enter position:");
+        setPosition(input.nextLine());
+        System.out.println("Please, enter age:");
+        setAge(input.nextInt());
+    }
+    public void printData(){
+        System.out.printf("Name: %s Position: %s Age: %d\n", getName(), getPosition(), getAge());
+    }
+}
---
+++ src/main/java/OOPConceptsHomeExercise/mainMethod.java
@@ -0,0 +1,11 @@
+package OOPConceptsHomeExercise;
+
+public class mainMethod {
+    public static void main(String[] arg){
+        Positions test = new Positions("test", "position", 12);
+
+        test.acceptDataFromConsole();
+        test.printData();
+
+    }
+}
