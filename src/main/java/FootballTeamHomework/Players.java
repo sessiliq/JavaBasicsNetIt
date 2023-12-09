43796c2eb8eb5822437405dbda3872c4361bcebc
penowa <penowa98@gmail.com>
2023-12-09 20:29:39 +0200

    football team homework

--- src/main/java/FootballTeamHomework/Players.java
+++ src/main/java/FootballTeamHomework/Players.java
@@ -1,34 +1,24 @@
 package FootballTeamHomework;
 
 public class Players {
+    //Variables for each player
     private String name;
     private double endurance;
     private double sprint;
     private double dribble;
     private  double passing;
     private double shooting;
-
-    private double currentRating;
     private double overallSkill;
 
+    //Setters and getters for the variables
     public double getOverallSkill() {
         return overallSkill;
     }
-
     public void setOverallSkill(double overallSkill) {
+        //overall skill is average value of all statistics
         overallSkill = ( (endurance + sprint + dribble + passing + shooting) / 5);
         this.overallSkill = overallSkill;
     }
-
-    public double getCurrentRating() {
-        return currentRating;
-    }
-
-    public void setCurrentRating(double currentRating) {
-        currentRating += overallSkill;
-        this.currentRating = currentRating;
-    }
-
     public String getName() {
         return name;
     }
@@ -77,6 +67,7 @@ public class Players {
         this.shooting = shooting;
     }
 
+    //Constructor for Player
     public Players (String name, double endurance, double sprint, double dribble, double passing, double shooting){
         setName(name);
         setEndurance(endurance);
@@ -84,5 +75,7 @@ public class Players {
         setDribble(dribble);
         setPassing(passing);
         setShooting(shooting);
+       //overall skill is set to 0 because is not accepted as a parameter. At the Set method will take appropriate value.
+        setOverallSkill(0);
     }
 }
--- src/main/java/FootballTeamHomework/Team.java
+++ src/main/java/FootballTeamHomework/Team.java
@@ -3,18 +3,22 @@ package FootballTeamHomework;
 import java.util.ArrayList;
 
 public class Team {
+    //variables for team's name and total rating
     private String name;
-    private int teamSize;
     private double rating;
+    //variable that keeps current sum of all skills of each player
+    private double currentRating;
 
-    ArrayList<Players> eachPlayer = new ArrayList<>();
+    //list with all the players
+    ArrayList<Players> allPlayers = new ArrayList<>();
 
+    //getter and setter for Name
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        if (!name.isEmpty()){
+        if (name.isEmpty()){
             System.out.println("Name must be entered!");
         }
         else {
@@ -22,28 +26,38 @@ public class Team {
         }
     }
 
+    //getter for the Rating. Rating is set in Add Players Method
     public double getRating() {
         return rating;
     }
 
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public int getTeamSize() {
-        return teamSize;
-    }
-
-    public void setTeamSize(int teamSize) {
-        this.teamSize = teamSize;
-    }
-
-    public Team (String name, int teamSize){
+    //Constructor for the team
+    public Team (String name){
         setName(name);
-        setTeamSize(teamSize);
     }
 
+    //mothod for adding members at the team. Here rating is set.
     public void addPlayerToTeam(Players eachPlayer){
+        allPlayers.add(eachPlayer);
+        currentRating+= eachPlayer.getOverallSkill();
+        rating = currentRating / allPlayers.size();
+    }
 
+    //Print count of team players
+    public void printTeamCountOfPlayers(){
+        System.out.printf("%s has %d players\n", getName(), allPlayers.size());
+    }
+
+    //Print the rating of the team
+    public void printTeamRating(){
+        System.out.printf("Rating of %s is %.2f\n", getName(), getRating());
+    }
+
+    //Print each player's skills.
+    public void printEachPlayersSkills(){
+        for (Players player: allPlayers) {
+            System.out.printf("The statistic of %s ",player.getName());
+            System.out.printf("is %.2f\n",player.getOverallSkill());
+        }
     }
 }
