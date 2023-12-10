bcbe77da7067497bf8c6f4e890bb5980ec45f267
penowa <penowa98@gmail.com>
2023-12-10 16:42:38 +0200

    compare 3 numbers

---
+++ src/main/java/Homework8/MainMethod.java
@@ -0,0 +1,9 @@
+package Homework8;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        System.out.println(TheBiggestNumber.biggestNumber(3,2,1));
+        System.out.println(TheBiggestNumber.biggestNumber(2,3,1));
+        System.out.println(TheBiggestNumber.biggestNumber(1,2,3));
+    }
+}
