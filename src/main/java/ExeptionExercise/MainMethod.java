2ed1c2dd79a4185e0599d9e4d501e24d04bb4bbe
penowa <penowa98@gmail.com>
2023-12-21 20:31:29 +0200

    exeption exercise

--- src/main/java/ExeptionExercise/MainMethod.java
+++ src/main/java/ExeptionExercise/MainMethod.java
@@ -6,20 +6,25 @@ import java.io.IOException;
 public class MainMethod {
     //If a method throws exception it must be declared in the method signature; in first method and in all that calls it
     public static void main(String[] args) throws IOException {
-        //try - here is the block of code that may throw exeption
+        //try - here is the block of code that may throw exception
         //in try-catch construction can be only one try block
         try{
             ExceptionsExercise.readFile("nanana");
-            // ExeptionsExercise.readFile("C:\\Users\\penow\\OneDrive\\Desktop\\ese.txt");
-          //  ExeptionsExercise.readFile("C:\\Users\\penow\\OneDrive\\Desktop\\ese2.txt");
+             //ExceptionsExercise.readFile("C:\\Users\\penow\\OneDrive\\Desktop\\ese.txt");
+           // ExceptionsExercise.readFile("C:\\Users\\penow\\OneDrive\\Desktop\\ese2.txt");
 
         }
         //in a try-catch construction can be many catch blocks
-        catch (FileNotFoundException fnfe){
-         System.out.println("Invalid file path name! Please, enter a valid one!");
-        }
-        catch (IOException test){
-            System.out.println("Invalid file path name! Please, enter a valid one!");
+       // catch (FileNotFoundException fnfe){
+       //  System.out.println("Invalid file path name! Please, enter a valid one!");
+      //  }
+        //declare exception and give it name
+      //  catch (IOException test){
+     //      System.out.println("Invalid file path name! Please, enter a valid one!");
+      //  }
+        //in this case we just throw the exeption
+        catch (IOException newException){
+            throw new IOException(newException);
         }
         //catch (IOException ioe) {
        // throw new RuntimeException(ioe);
